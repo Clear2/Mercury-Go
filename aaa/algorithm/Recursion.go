471fe4e0c314f3d11faf4b6653fc9ef0c6a6a0ec
zhangjiaxu01 <zhangjiaxu01@corp.netease.com>
2022-03-08 09:16:12 +0800

    climb stairs

--- aaa/algorithm/Recursion.go
+++ aaa/algorithm/Recursion.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(climbStairs1(3))
+}
+
+// Climbing Stairs
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+	count := climbStairs(n-1) + climbStairs(n-2)
+	return count
+}
+
+// Memorization
+func climbStairs1(n int) int {
+	memo := map[int]int{1: 1, 2: 2}
+	return memoClimb(n, memo)
+}
+func memoClimb(n int, memo map[int]int) int {
+	if v, ok := memo[n]; ok {
+		return v
+	}
+	memo[n] = memoClimb(n-1, memo) + memoClimb(n-2, memo)
+	return memo[n]
 }
