d41b05f8e246174a16f29216369a0a16a8209a89
zhangjiaxu01 <zhangjiaxu01@corp.netease.com>
2022-03-10 09:13:18 +0800

    merge

--- aaa/algorithm/Array.go
+++ aaa/algorithm/Array.go
@@ -6,21 +6,21 @@ import (
 )
 
 // 合并两个数组合并以后有序
-func merge(nums1 []int, m int, nums2 []int, n int) {
-	var p1 = m - 1
-	var p2 = n - 1
-	p := m + n - 1
-	for p2 >= 0 {
-		if p1 >= 0 && nums1[p1] > nums2[p2] {
-			nums1[p] = nums1[p1]
-			p1--
-		} else {
-			nums1[p] = nums2[p2]
-			p2--
-		}
-		p--
-	}
-}
+//func merge(nums1 []int, m int, nums2 []int, n int) {
+//	var p1 = m - 1
+//	var p2 = n - 1
+//	p := m + n - 1
+//	for p2 >= 0 {
+//		if p1 >= 0 && nums1[p1] > nums2[p2] {
+//			nums1[p] = nums1[p1]
+//			p1--
+//		} else {
+//			nums1[p] = nums2[p2]
+//			p2--
+//		}
+//		p--
+//	}
+//}
 
 // 原地修改数组
 func removeDuplicates(nums []int) int {
@@ -55,6 +55,19 @@ func main() {
 	//fmt.Println(num1, num2)
 	length := removeDuplicates(num1)
 	fmt.Println(length)
+}
+
+// Merge Sorted Array
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n > 0 {
+		if m <= 0 || nums2[n-1] >= nums1[m-1] {
+			nums1[m+n-1] = nums2[n-1]
+			n = n - 1
+		} else {
+			nums1[m+n-1] = nums1[m-1]
+			m -= 1
+		}
+	}
 }
 
 // 数组不重复全排列 -> https://leetcode.com/problems/permutations-ii/
